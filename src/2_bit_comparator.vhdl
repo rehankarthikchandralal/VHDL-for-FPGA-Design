57d4b189c879c057317a59fccf9796a823ba29af
Rehan <rehankarthik12@gmail.com>
2024-11-26 13:53:07 +0100

    added comments

--- src/2_bit_comparator.vhdl
+++ src/2_bit_comparator.vhdl
@@ -3,12 +3,12 @@ use ieee.std_logic_1164.all;
 
 entity comparator_2_bit is
   port (
-    a,b : in  std_logic_vector(1 downto 0);
-    c: out std_logic
+    a, b : in  std_logic_vector(1 downto 0);  -- Inputs
+    c    : out std_logic                     -- Output
   );
 end entity comparator_2_bit;
 
 architecture Behavioral of comparator_2_bit is
 begin
-  c <= '1' when a = b else '0';
+  c <= '1' when (a = b) else '0';          -- Comparator logic
 end architecture Behavioral;
